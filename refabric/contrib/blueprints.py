abbc61db107a0da3289aa807295b3aa3b740f2e7
Jonas Lundberg <jonas@5monkeys.se>
2014-09-03 15:48:41 +0200

    Add support to download templates through blueprint

--- refabric/contrib/blueprints.py
+++ refabric/contrib/blueprints.py
@@ -3,6 +3,7 @@ import importlib
 import os
 from functools import partial
 
+from fabric.contrib import files
 from fabric.state import env
 
 from .templates import upload
@@ -26,6 +27,17 @@ class Blueprint(object):
     def get(self, setting, default=None):
         return self.settings(setting, default=default)
 
+    def get_template_path(self, relative_path, role=None):
+        deploy_root = env['real_fabfile']
+        path = [os.path.dirname(deploy_root), 'templates']
+        if not role:
+            role = env.roles[0] if env.roles else None  # TODO: Is this safe?
+            if role:
+                path.append(role)
+        path.append(self.name)
+        path.append(relative_path)
+        return os.path.join(*path)
+
     def get_template_loader(self):
         role = env.roles[0] if env.roles else None  # TODO: Is this safe?
         return BlueprintTemplateLoader(self.blueprint, role=role)
@@ -47,6 +59,21 @@ class Blueprint(object):
         with sudo('root'):
             return upload(template, destination, context=context, user=user, group=group,
                           jinja_env=jinja_env)
+
+    def download(self, remote_path, rel_destination_path, role=None):
+        """
+        Currently only supports downloading single file
+        """
+        destination_path = self.get_template_path(rel_destination_path)
+
+        # Append filename to destination if missing
+        filename = os.path.basename(remote_path)
+        if os.path.basename(destination_path) != filename:
+            destination_path = os.path.join(destination_path, filename)
+
+        with sudo('root'):
+            info('Downloading {} -> {}', remote_path, destination_path)
+            files.get(remote_path, destination_path)
 
 
 class BlueprintTemplateLoader(jinja2.FileSystemLoader):
