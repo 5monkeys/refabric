a736797ec7d313729b31d153c38924d801c6bb1e
Hannes Ljungberg <hannes@5monkeys.se>
2014-11-24 15:32:27 +0100

    Save checksums of uploaded files and validate them on further uploads.

--- refabric/contrib/blueprints.py
+++ refabric/contrib/blueprints.py
@@ -59,12 +59,6 @@ class Blueprint(object):
         return text
 
     def upload(self, template, destination, context=None, user=None, group=None):
-        from_name = os.path.basename(template.rstrip(os.path.sep))
-        to_name = os.path.basename(destination.rstrip(os.path.sep))
-        if to_name != from_name:
-            info('Uploading templates: {} to {}', template, to_name)
-        else:
-            info('Uploading templates: {}', template)
         jinja_env = self.get_jinja_env()
         context = context or {}
         context.setdefault('host', env.host_string)
--- refabric/contrib/templates.py
+++ refabric/contrib/templates.py
@@ -1,11 +1,12 @@
 import os
+from fabric.context_managers import quiet
 import jinja2
 import shutil
 import tempfile
 from functools import partial
 
 from fabric.colors import magenta
-from fabric.operations import put
+from fabric.operations import put, prompt
 from fabric.utils import abort, puts, indent, warn
 
 from ..context_managers import silent, abort_on_error
@@ -38,8 +39,20 @@ class FileDescriptor(str):
 
 def upload(source, destination, context=None, user=None, group=None, jinja_env=None):
     """
+    Will render and upload a local file or folder to a destination file or folder.
+    Destination is handled as a folder if it ends with a slash,
+    otherwise as a file and therefore could be used to rename template on upload.
 
+    :param source: Path to local file or folder.
+    :param destination: Absolute destination path. Folders always end with a slash.
+    :param context: Context to render source template.
+    :param user: User owner of destination file/folder
+    :param group: Group owner of destination file/folder
+    :param jinja_env: Jinja2 Environment to load templates from.
+    :return: List of updated files
     """
+    info('Uploading templates: {}', os.path.basename(source.rstrip(os.path.sep)))
+
     tmp_dir = tempfile.mkdtemp()
     try:
         # TODO: Handle None template_loader
@@ -69,6 +82,24 @@ def upload(source, destination, context=None, user=None, group=None, jinja_env=N
             # Render template
             context = context or {}
             context['n'] = os.path.splitext(os.path.basename(template))[0]
+
+            abs_destination_file = destination
+            if destination.endswith(os.path.sep):
+                abs_destination_file = os.path.join(destination, rel_template_path)
+
+            # Check md5sum of file present on sure with checksum generated on last upload
+            with quiet():
+                md5_output = run('md5sum -c --status {file}.md5 || test ! -e {file}.md5'.format(
+                    file=abs_destination_file))
+            if md5_output.return_code != 0:
+                warn('Template "{}" checksum mismatch. File changed since last upload.'.format(
+                    template))
+                answer = prompt('Type "yes" to overwrite, or "no" to skip:',
+                                default='no', validate='yes|no')
+                if answer == 'no':
+                    # Skip render and upload of md5 mismatched file
+                    continue
+
             try:
                 text = jinja_env.get_template(template).render(**context or {})
                 text = text.encode('utf-8')
@@ -107,7 +138,14 @@ def upload(source, destination, context=None, user=None, group=None, jinja_env=N
             updated_files = [line.strip() for line in updated.stdout.split('\n') if line]
             if updated_files:
                 for updated_file in updated_files:
-                    info(indent('Uploaded: {}'), updated_file)  # TODO: Handle renaming
+                    updated_file_path = destination
+                    if destination.endswith(os.path.sep):
+                        updated_file_path = os.path.join(destination, updated_file)
+                    else:
+                        updated_file = os.path.basename(destination)
+                    info(indent('Uploaded: {}'), updated_file)
+                    # Create md5 checksum of uploaded file
+                    run('md5sum {file} > {file}.md5'.format(file=updated_file_path))
             else:
                 puts(indent('(no changes found)'))
 
