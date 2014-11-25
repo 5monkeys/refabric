cb2e2958930ed19ff5b85cffaf79ec5374d4a0b2
Hannes Ljungberg <hannes@5monkeys.se>
2014-11-25 17:51:43 +0100

    Pass kwargs to fabric_run

--- refabric/operations.py
+++ refabric/operations.py
@@ -5,22 +5,24 @@ from .context_managers import silent
 __all__ = ['run']
 
 
-def run(command, shell=True, pty=True, combine_stderr=None, use_sudo=False, user=None):
+def run(command, shell=True, pty=True, combine_stderr=None, use_sudo=False, user=None, **kwargs):
     use_sudo = use_sudo or user is not None or env.get('use_sudo')
 
     if not use_sudo:
-        return fabric_run(command, shell=shell, pty=pty, combine_stderr=combine_stderr)
+        return fabric_run(command, shell=shell, pty=pty, combine_stderr=combine_stderr, **kwargs)
 
     else:
         user = user or env.get('sudo_user', env.user)
         # Make SSH agent socket available to the sudo user
         with silent():
-            fabric_sudo('chown -R {}: $(dirname $SSH_AUTH_SOCK)'.format(user), user='root')
+            fabric_sudo('chown -R {}: $(dirname $SSH_AUTH_SOCK)'.format(user), user='root',
+                        **kwargs)
 
         if user == env.user:
             user = None
 
-        return fabric_sudo(command, shell=shell, pty=pty, combine_stderr=combine_stderr, user=user)
+        return fabric_sudo(command, shell=shell, pty=pty, combine_stderr=combine_stderr,
+                           user=user, **kwargs)
 
 
 ### Monkey patch fabric
