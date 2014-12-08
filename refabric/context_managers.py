a87d84170c4713d4345a5b44f1002b3bd2421f22
Jonas Lundberg <jonas@5monkeys.se>
2014-12-08 15:34:55 +0100

    Remove shell_env context manager

--- refabric/context_managers.py
+++ refabric/context_managers.py
@@ -1,10 +1,8 @@
 # coding=utf-8
 from contextlib import contextmanager
 
-from fabric.context_managers import settings, hide, prefix
+from fabric.context_managers import settings, hide
 from fabric.state import env
-
-__all__ = ['get_sudo_context', 'sudo', 'only_messages', 'prefix']
 
 
 @contextmanager
@@ -16,13 +14,3 @@ def sudo(user=None):
 silent = lambda *h: settings(hide('commands', *h), warn_only=True)
 hide_prefix = lambda: settings(output_prefix=False)
 abort_on_error = lambda: settings(warn_only=False)
-
-
-@contextmanager
-def shell_env(**env_vars):
-    orig_shell = env['shell']
-    env_vars_str = ' '.join('{0}={1}'.format(key, value)
-                            for key, value in env_vars.items())
-    env['shell'] = '{0} {1}'.format(env_vars_str, orig_shell)
-    yield
-    env['shell'] = orig_shell
