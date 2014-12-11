b0034e96a0759f58234ca80c54f91e6394e777ba
Jonas Lundberg <jonas@5monkeys.se>
2014-12-11 13:12:56 +0100

    Move fabric patching to bootstrap

--- refabric/bootstrap.py
+++ refabric/bootstrap.py
@@ -1,29 +1,27 @@
 __all__ = ['bootstrap']
 
 
-# Monkey patch fabric
-from .patch import patch
-patch('fabric.operations.run')
-patch('fabric.operations.sudo', 'refabric.operations.run')
-patch('fabric.state.switch_env')
-patch('fabric.tasks._execute')
-patch('fabric.tasks.Task:get_hosts_and_effective_roles')
-patch('fabric.utils._AttributeDict')
-
-
-# Reload fabric's run/sudo internal import references to patched version
-import fabric.api
-import fabric.contrib.files
-import fabric.contrib.project
-import fabric.operations
-for m in (fabric.api, fabric.contrib.files, fabric.contrib.project):
-    m.run = m.sudo = fabric.operations.run
-
-
 def bootstrap():
     """
     Define global blueprint tasks, load configured blueprints and apply role definitions.
     """
+    # Monkey patch fabric
+    from .patch import patch
+    patch('fabric.operations.run')
+    patch('fabric.operations.sudo', 'refabric.operations.run')
+    patch('fabric.state.switch_env')
+    patch('fabric.tasks._execute')
+    patch('fabric.tasks.Task:get_hosts_and_effective_roles')
+    patch('fabric.utils._AttributeDict')
+
+    # Reload fabric's run/sudo internal import references to patched version
+    import fabric.api
+    import fabric.contrib.files
+    import fabric.contrib.project
+    import fabric.operations
+    for m in (fabric.api, fabric.contrib.files, fabric.contrib.project):
+        m.run = m.sudo = fabric.operations.run
+
     import fabric.state
     from fabric.decorators import task
 
