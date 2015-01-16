4a29b5169524205bfa50a89379f4439d0de40296
Jonas Lundberg <jonas@5monkeys.se>
2015-01-16 14:18:55 +0100

    Add role context manager setting role and definitions

--- refabric/context_managers.py
+++ refabric/context_managers.py
@@ -2,12 +2,20 @@ from contextlib import contextmanager
 
 from fabric.context_managers import settings, hide
 from fabric.state import env
+from refabric.state import apply_role_definitions
 
 
 @contextmanager
 def sudo(user=None):
     with settings(sudo_user=user or env.sudo_user or env.user, use_sudo=True):
         yield
+
+
+@contextmanager
+def role(name):
+    with settings(roles=[name]):
+        yield
+        apply_role_definitions(None)
 
 
 silent = lambda *h: settings(hide('commands', *h), warn_only=True)
