5236608f421f5c6c5eec3cfaafaf78418f1a91e4
Hannes Ljungberg <hannes@5monkeys.se>
2015-12-01 12:41:11 +0100

    Actually validate all hosts

--- refabric/tasks.py
+++ refabric/tasks.py
@@ -25,12 +25,12 @@ class Task(object):
                 print("{i}. {host}".format(i=i, host=host))
 
             def validate_hosts(host_input):
-                check = lambda s: s.isdigit() and int(s) in range(len(all_hosts))
+                check = lambda s: s.isdigit() and int(s) in range(len(all_hosts) + 1)
                 if not all(map(check, host_input.split(','))):
                     raise Exception('Invalid host {!r}'.format(host_input))
                 else:
                     return host_input
-                
+
             host_choice = prompt(green('Select host(s)'), default='0', validate=validate_hosts)
             indices = map(int, host_choice.split(','))
             if len(indices) > 1 or indices[0] > 0:
